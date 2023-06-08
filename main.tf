5c12149763a394c98577eb0ce38c72705f752805
lakshmikanthisl <lakshmikanthisl@gmail.com>
2023-06-08 06:43:53 +0530

    Adding Backend

--- main.tf
+++ main.tf
@@ -8,6 +8,15 @@ terraform {
   }
 }
 
+  #Terraform State Storage to Azure Storage container
+backend "azurerm" {
+    resource_group_name  = "rg1"
+    storage_account_name = "terrafromstate2100806" #Name of Azure storage account to be specific across Azure
+    container_name       = "tfstatefiles"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   #skip_provider_registration = "true" #Optinally used (Error ensuring Resource Providers are registered.) 
   features {
