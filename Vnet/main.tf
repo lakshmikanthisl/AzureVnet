17286f80ffbb050e699fea50e76550216c6c3773
lakshmikanthisl <67961725+lakshmikanthisl@users.noreply.github.com>
2023-06-11 17:12:45 +0530

    Commented Backend

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -8,14 +8,14 @@ terraform {
   }
 }
 
-  #Terraform State Storage to Azure Storage container
+/*  #Terraform State Storage to Azure Storage container
 backend "azurerm" {
     resource_group_name  = "rg1"
     storage_account_name = "terrafromstate2100806" #Name of Azure storage account to be specific across Azure
     container_name       = "tfstatefiles"
     key                  = "terraform.tfstate"
   }
-}
+*/
 
 provider "azurerm" {
   #skip_provider_registration = "true" #Optinally used (Error ensuring Resource Providers are registered.) 
