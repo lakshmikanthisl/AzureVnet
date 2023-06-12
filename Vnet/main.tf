5f49d07698c4c100d083a056ff86d90926583624
lakshmikanthisl <lakshmikanthisl@gmail.com>
2023-06-12 11:39:15 +0530

    Update rg on Vnet

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -6,28 +6,9 @@ terraform {
       version = ">= 2.0"
     }
   }
-<<<<<<< HEAD
   backend "azurerm" {}
 }
 
-/*  #Terraform State Storage to Azure Storage container
-backend "azurerm" {
-    resource_group_name  = "rg1"
-    storage_account_name = "terrafromstate2100806" #Name of Azure storage account to be specific across Azure
-    container_name       = "tfstatefiles"
-    key                  = "terraform.tfstate"
-=======
-  
-  #Terraform State Storage to Azure Storage container
-  backend "azurerm" {
-  #  resource_group_name  = "rg1"
-  #  storage_account_name = "terrafromstate2100806" #Name of Azure storage account to be specific across Azure
-  #  container_name       = "tfstatefiles"
-  #  key                  = "terraform.tfstate"
->>>>>>> d3cdb6cac617374084d8f82048b289ffc7cd6deb
-  }
-
-*/
 provider "azurerm" {
   #skip_provider_registration = "true" #Optinally used (Error ensuring Resource Providers are registered.) 
   features {
--- Vnet/rg.tf
+++ Vnet/rg.tf
@@ -1,3 +1,9 @@
+module "azure_resource_group" {
+  source = "https://github.com/lakshmikanthisl/AzureVnet/tree/main/Resource%20Group"
+  location = "eastus"
+  name = "RG-1"
+}
+/*
 resource "azurerm_resource_group" "myrg" {
   #name = "myrg-1"
   #name = "${var.business_unit}-${var.environment}-${var.resource_group_name}"
@@ -6,3 +12,5 @@ resource "azurerm_resource_group" "myrg" {
   location = var.resource_group_location
   tags     = local.common_tags
 }
+
+*/
